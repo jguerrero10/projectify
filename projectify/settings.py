96e77d8f8edc2bbbcf1b616f2bb458b19c352747
Joel Guerrero <joelgur10@hotmail.com>
2022-04-02 14:30:55 -0500

    setting is updated

--- projectify/settings.py
+++ projectify/settings.py
@@ -122,17 +122,11 @@ WSGI_APPLICATION = 'projectify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
 }
 
 # Password validation
